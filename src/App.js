76960c7fc19bbb07e67ae32f9d4161379bd802f7
Al Amin Khan <alaminkhanshakil@gmail.com>
2023-09-22 06:02:19 +0600

    Add navbar with router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Books from './components/Books';
+import Navbar from './components/Navbar';
+import Categories from './components/Categories';
 
 function App() {
   return (
-    <div className="App">
-      <Books />
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </>
   );
 }
 
