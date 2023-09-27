571277cb3b46c4f464c45b63dd5f7185acf646a5
Al Amin Khan <alaminkhanshakil@gmail.com>
2023-09-28 02:09:36 +0600

    Fix bookSlice error

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,20 +1,20 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   books: [],
-}
+};
 
 const bookSlice = createSlice({
-  name: books,
+  name: 'books',
   initialState,
   reducers: {
     addBook: (state, action) => {
       state.books.push(action.payload);
     },
     removeBook: (state, action) => {
-      state.books.filter((book) => book.id !== action.payload)
-    }
-  }
+      state.books.filter((book) => book.id !== action.payload);
+    },
+  },
 });
 
 export const { addBook, removeBook } = bookSlice.actions;
