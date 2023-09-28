6997cf4da21935a58a62370ebdab2958fc124982
Al Amin Khan <alaminkhanshakil@gmail.com>
2023-09-28 15:08:48 +0600

    Get booklist from store

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -1,26 +1,31 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 import DeleteBook from './DeleteBook';
 
-const BookItem = ({ books }) => (
-  <ul className="books-container">
-    {books.map((book) => (
-      <li className="book-item" key={book.id}>
-        <h2 className="book-title">{book.title}</h2>
-        <p className="book-author">{book.author}</p>
-        <DeleteBook />
-      </li>
-    ))}
-  </ul>
-);
+const BookItem = () => {
+  const books = useSelector((state) => state.book.books);
 
-BookItem.propTypes = {
-  books: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      title: PropTypes.string.isRequired,
-      author: PropTypes.string.isRequired,
-    }),
-  ).isRequired,
+  return (
+    <ul className="books-container">
+      {books.map((book) => (
+        <li className="book-item" key={book.id}>
+          <h2 className="book-title">{book.title}</h2>
+          <p className="book-author">{book.author}</p>
+          <DeleteBook />
+        </li>
+      ))}
+    </ul>
+  );
 };
+
+// BookItem.propTypes = {
+//   books: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.number.isRequired,
+//       title: PropTypes.string.isRequired,
+//       author: PropTypes.string.isRequired,
+//     }),
+//   ).isRequired,
+// };
 
 export default BookItem;
--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,31 +1,11 @@
 import AddBook from './AddBook';
 import BookItem from './BookItem';
 
-const Books = () => {
-  const books = [
-    {
-      id: 1,
-      title: 'Days of Dhaka',
-      author: 'Kazi Nazrul Islam',
-    },
-    {
-      id: 2,
-      title: 'Days of Kalkata',
-      author: 'Rabindronath Thakur',
-    },
-    {
-      id: 1,
-      title: 'One Day One Night',
-      author: 'Jhon Due',
-    },
-  ];
-
-  return (
-    <div className="books-wrapper">
-      <BookItem books={books} />
-      <AddBook />
-    </div>
-  );
-};
+const Books = () => (
+  <div className="books-wrapper">
+    <BookItem />
+    <AddBook />
+  </div>
+);
 
 export default Books;
